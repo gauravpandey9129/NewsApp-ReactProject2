4876d72cc193e3a2e6e101ee767589603645488c
gauravpandey9129 <gaurav912959@gmail.com>
2024-02-02 17:19:19 +0530

    using propes in js

--- newsapp/src/Components/NewsItems.js
+++ newsapp/src/Components/NewsItems.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
-export default class NewsItems extends Component {
+export default  class NewsItems extends Component {
+
   render() {
-    return (
-      <div>this is news for you </div>
-    )
-  }
+    let {title,discription,urlImage,newsurl}=this.props;
+       return (
+          <div>
+          <div  className="card" style={{width: '18rem'}}>
+          <img src={urlImage}  className="card-img-top" alt="..."/>
+          <div  className="card-body">
+          <h5  className="card-title">{title}</h5>
+          <p  className="card-text">{discription}</p>
+          <a href={newsurl}  className="btn btn-primary">More Details</a>
+           </div>
+           </div>
+            </div>
+          )
+     }
+
 }
