a42ed9e38e84c23bb79dd4b2c114dd3bb5983337
gauravpandey9129 <gaurav912959@gmail.com>
2024-02-02 12:56:41 +0530

    added News Component

---
+++ newsapp/src/Components/News.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>This is news component
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+
+                
+      </div>
+    )
+  }
+}
+
+export default News
