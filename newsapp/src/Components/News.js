00863b1ec75da7a4ca6730cce2ee5ff0bd70f851
gauravpandey9129 <gaurav912959@gmail.com>
2024-02-04 10:15:38 +0530

    added next&prev button with functionality

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -2,55 +2,76 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
 export class News extends Component {
-    articles=[
-        {
-        "source": {
-          "id": "espn-cric-info",
-          "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-      },
-      {
-        "source": {
-          "id": "espn-cric-info",
-          "name": "ESPN Cric Info"
-        },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      }
-    ]
+   
   
     constructor(){
         super();
         this.state={
-            articles:this.articles,
-            loading:false
+            articles:[],
+            loading:false,
+            page:1,
         }
     }
+
+   async componentDidMount(){
+        let url="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ee613bef369d4945a28b876a578f6b44&pagesize=15"
+        let data= await fetch(url);
+        let jdata= await data.json();
+        this.setState({articles:jdata.articles,totalResults:jdata.totalResults})
+
+    }
+       handlePrevClick=async()=>{
+        let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ee613bef369d4945a28b876a578f6b44&page=${this.state.page-1}&pagesize=15`;
+        let data= await fetch(url);
+        let jdata= await data.json();
+        this.setState({articles:jdata.articles})
+  
+        this.setState({
+         page: this.state.page-1,
+
+        })
+
+       }
+
+
+        handleNextClick=async()=>{
+
+            if(this.state.page+1>Math.ceil(this.state.totalResults/15)){
+
+            }else{
+
+            
+       
+                let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ee613bef369d4945a28b876a578f6b44&page=${this.state.page+1}&pagesize=15`;
+               let data= await fetch(url);
+               let jdata= await data.json();
+               this.setState({articles:jdata.articles})
+      
+               this.setState({
+               page: this.state.page+1,
+
+            })
+        }
+        }
+
+
   render() {
     return (
        <div  className="container my-3 mx-4">
-          <h2>NewsBook Top News Of The Day</h2>
+          <h2 className="text-center fw-bold bg-warning fst-italic border border-dark"> Top News Of The Day </h2>
           <div className="row">
-              {this.state.articles.map((element)=>{
-                 return <div className="col-md-4" key={element.url}>
-                <NewsItems title={element.title.slice(0,45)} discription={element.description.slice(0,85)}  urlImage={element.urlToImage} newsurl={element.url}/>
+              {this.state.articles.map((elements)=>{
+                 return <div className="col-md-4" key={elements.url}>
+                <NewsItems title={elements.title?elements.title.slice(0,45):" "} discription={elements.description?elements.description:" "}  urlImage={elements.urlToImage?elements.urlToImage:"https://images.wsj.net/im-919794/social"} newsurl={elements.url?elements.url:"https://www.wsj.com/real-estate/luxury-homes/100-million-dollar-homes-8ee49b5e"}/>
                 </div>
               })}
              
             </div>
-        
+
+        <div className="container d-flex justify-content-between">
+            <button  disabled={this.state.page<=1} type="button" className="btn btn-dark my-3" onClick={this.handlePrevClick}> &larr;Previous</button>
+            <button type="button" className="btn btn-dark my-3" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
        
 
                 
