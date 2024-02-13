ac9f5687f64fa8662b3986a153e4a81dfc785c42
gauravpandey9129 <gaurav912959@gmail.com>
2024-02-13 15:40:26 +0530

    RouterAdded

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar'
 import News from './Components/News'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
-        <News pagesize={9}/>
+      <div  >
+        
+       <BrowserRouter>
+       <Navbar/>
+       <Routes>
+        <Route path='/' element={<News/>}></Route>
+       </Routes>
+       
+       </BrowserRouter>
       </div>
     )
   }
--- newsapp/src/Components/Navbar.js
+++ newsapp/src/Components/Navbar.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react'
+import { Link,NavLink} from 'react-router-dom'
+
+
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg text-light bg-info border border-warning p-3 ">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">NewsBook</a>
-           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/about">About</a>
-            </li>
-        
-        
-      </ul>
-    </div>
-  </div>
-</nav>
+      <div >
+       <ul className=" nav justify-content-center  bg-dark-subtle">
+       <li className="nav-item">
+         <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Contact Us</Link>
+        </li>
+        <li className="nav-item">
+       
+        </li>
+</ul>
       </div>
     )
   }
